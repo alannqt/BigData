7e95f2e7d790919b5f234dab0c9e5e95600b9f65
mohameddhameemm <m.md.mohamed@gmail.com>
2020-04-08 11:41:19 +0800

    Updated commit with data file

--- src/main/scala/IngestWorkLog.scala
+++ src/main/scala/IngestWorkLog.scala
@@ -7,17 +7,27 @@ import org.apache.spark.sql.functions._
 
 import org.apache.spark.sql.types._
 
+import org.apache.log4j.Logger
+import org.apache.log4j.Level
+
 object IngestWorkLog {
   def main(args: Array[String]): Unit = {
-    val inputFilePath = "file:/Users/mohameddhameemm/IdeaProjects/bigdata/Data/WorkLog/JobSummary*.csv"
+    Logger.getLogger("org").setLevel(Level.OFF)
+    val inputFilePath = "file:/Users/mohameddhameemm/IdeaProjects/bigdata/Data/WorkLog/JobSummary*.txt"
     val conf = new SparkConf().setAppName("WorkLog").setMaster("local[2]")
     val context = new SparkContext(conf)
     val spark = SparkSession.builder().appName("WorkLog").getOrCreate()
     val df = spark.read
       .option("inferschema","true")
       .option("header","true")
+      .option("delimiter", "\t")
       .csv(inputFilePath)
-    df.show()
+    df.show(truncate = false)
     df.printSchema()
+    df.write.format("csv").save("./test1")
+    println("COUNT --> "+df.count())
+    //Lets try to get the unique crane list out from the list
+    val df_unique_list = df.select("Location").dropDuplicates()
+    df_unique_list.show(truncate = false)
   }
 }
